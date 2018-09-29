1065a4fd73b315821bc808c561323972aa769485
Roman Kovtun <gyto23@gmail.com>
2018-09-29 16:00:39 -0500

    add lab for week 4 fix the issue with git ignore

---
+++ CIS355A/lab_week_4/src/com/roman/week4lab/Stock.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.roman.week4lab;
+
+/**
+ *
+ * @author Roman K
+ */
+
+public class Stock {
+    // attrubuters
+    
+    private String companyName;
+    private Double numberOfShares;
+    private Double purchasePrice;
+    private Double currentPrice;
+    
+    // constractors
+    
+    /// Default constractor
+    public Stock() {
+        companyName = "N/A";
+        numberOfShares = 0.0;
+        purchasePrice = 0.0;
+        currentPrice = 0.0;
+    }
+    
+    public Stock(String companyName, Double numberOfShares, Double purchasePrice, Double currentPrice) {
+        this.companyName = companyName;
+        this.numberOfShares = numberOfShares;
+        this.purchasePrice = purchasePrice;
+        this.currentPrice = currentPrice;
+    }
+    
+    // behaviors
+    public double calculateProfitLoss()
+    {
+        return numberOfShares * (currentPrice - purchasePrice);
+    }
+
+    @Override
+    public String toString() {
+        return companyName;
+    }
+    
+    // accessors and mutators
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public Double getNumberOfShares() {
+        return numberOfShares;
+    }
+
+    public void setNumberOfShares(Double numberOfShares) {
+        this.numberOfShares = numberOfShares;
+    }
+
+    public Double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(Double purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public Double getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(Double currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+}
